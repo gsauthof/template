dcffae4807ca5c866ab916d3e5ec158b1fffe3ce
Georg Sauthoff <mail@georg.so>
2016-09-28 16:05:38 +0200

    add main templates for C/C++

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// POSIX
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+int main(int argc, char **argv)
+{
+  puts("Hello World!");
+  return 0;
+}
---
+++ main.cc
@@ -0,0 +1,65 @@
+//#include <algorithm>
+//#include <array>
+//#include <cassert>
+//#include <cctype>
+//#include <deque>
+//#include <functional>
+#include <iostream>
+//#include <limits>
+//#include <map>
+//#include <memory>
+//#include <ostream>
+//#include <set>
+//#include <stack>
+//#include <stdexcept>
+#include <string>
+//#include <unordered_map>
+//#include <unordered_set>
+//#include <utility>
+//#include <vector>
+
+//#include <boost/algorithm/string.hpp>
+//#include <boost/algorithm/string/classification.hpp>
+//#include <boost/algorithm/string/case_conv.hpp>
+//#include <boost/algorithm/string/erase.hpp>
+//#include <boost/algorithm/string/finder.hpp>
+//#include <boost/algorithm/string/find_iterator.hpp>
+//#include <boost/algorithm/string/predicate.hpp>
+//#include <boost/algorithm/string/replace.hpp>
+//#include <boost/algorithm/string/split.hpp>
+//#include <boost/date_time/posix_time/posix_time.hpp>
+//#include <boost/date_time/posix_time/posix_time_io.hpp>
+//#include <boost/date_time/posix_time/posix_time_type.hpp>
+//#include <boost/date_time/posix_time/ptime.hpp>
+//#include <boost/endian/conversion.hpp>
+//#include <boost/exception/all.hpp>
+//#include <boost/filesystem.hpp>
+//#include <boost/format.hpp>
+//#include <boost/functional/hash.hpp>
+//#include <boost/icl/interval_set.hpp>
+//#include <boost/interprocess/file_mapping.hpp>
+//#include <boost/interprocess/mapped_region.hpp>
+//#include <boost/interprocess/streams/vectorstream.hpp>
+//#include <boost/intrusive/rbtree_algorithms.hpp>
+//#include <boost/intrusive/set.hpp>
+//#include <boost/io/ios_state.hpp>
+//#include <boost/lexical_cast.hpp>
+//#include <boost/logic/tribool.hpp>
+//#include <boost/log/trivial.hpp>
+//#include <boost/log/utility/setup/console.hpp>
+//#include <boost/multiprecision/cpp_dec_float.hpp>
+//#include <boost/optional.hpp>
+//#include <boost/predef.h>
+//#include <boost/property_tree/json_parser.hpp>
+//#include <boost/regex.hpp>
+//#include <boost/spirit/include/qi_auto.hpp>
+//#include <boost/spirit/include/qi_numeric.hpp>
+//#include <boost/spirit/include/qi_parse.hpp>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  cout << "Hello World!\n";
+  return 0;
+}
