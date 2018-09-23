1f784bf5af6fb663936b8678c58e49e080c95eef
Georg Sauthoff <mail@georg.so>
2018-09-23 22:56:55 +0200

    add notes regarding Catch2, parametrizing

--- catch_ut.cc
+++ catch_ut.cc
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-TEST_CASE("module" "unique name", "[tag1][foo][bar]" )
+TEST_CASE("module " "unique name", "[tag1][foo][bar]" )
 {
   CHECK(1 > 0);
   REQUIRE(23 == 100 - 77);
@@ -12,4 +12,22 @@ TEST_CASE("module" "unique name", "[tag1][foo][bar]" )
   string s;
   CHECK_NOTHROW(string("hello").size());
   CHECK_THROWS_AS(s.at(1), std::logic_error);
+}
+
+static void test_file(const char *filename)
+{
+    CHECK(string(filename).empty() == false);
+}
+static const array<const char*, 2> filenames = { "foo", "bar" };
+// run with -s to get some details on the sub-cases
+// run with -c to filter a certain case
+TEST_CASE("module " "parametrized test", "[tag1]" )
+{
+  size_t i = 0;
+  for (auto filename : filenames) {
+      SECTION(string("compare-") + to_string(i)) {
+          test_file(filename);
+      }
+      ++i;
+  }
 }
--- catch_ut_main.cc
+++ catch_ut_main.cc
@@ -1,6 +1,13 @@
+// with Catch2 cloned into a subdirectory compile with:
+// CPPFLAGS += -ICatch2/single_include
+
 // use default main:
 #define CATCH_CONFIG_MAIN
-#include <catch/catch.hpp>
+
+// System-wide location for Catch _and_ Catch2 on Fedora:
+// #include <catch/catch.hpp>
+// With Catch2 cloned into a subdirectory:
+#include <catch2/catch.hpp>
 
 /*
 // use custom main:
