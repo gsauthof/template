04bb0153d1dc3b8ec06123478326ab2ccde0510f
Georg Sauthoff <mail@gms.tf>
2020-04-11 18:34:50 +0200

    Add Python 2 boilerplate to enable modern features
    of course, this also simplifies code a bit that neeeds to support
    Python 2 (say 2.7) and Python 3

---
+++ py2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# ↳ tell Python 2 to read source file as UTF-8 instead of some locale
+#   cf. https://www.python.org/dev/peps/pep-0263/
+
+# enable Python 3 features that are available but are disabled by
+# default in Python 2 (cf. https://docs.python.org/2.7/library/__future__.html)
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
